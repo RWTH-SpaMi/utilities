51013c1b5bc60526d8c06dd96102f2bf600a2119
ab5424 <alexander.bonkowski@rwth-aachen.de>
2024-10-22 10:59:44 +0200

    Add ml potentials example notebook

--- ML-Potentials/SEVENNET/python/01_minimum_example.py
+++
@@ -1,8 +0,0 @@
-"""Minimal example of using the SEVENNET calculator with ASE."""
-
-from ase.build import bulk
-from sevenn.sevennet_calculator import SevenNetCalculator
-
-structure_ase = bulk("Cu", "fcc", a=3.6)
-structure_ase.calc = SevenNetCalculator("7net-0")
-structure_ase.get_potential_energy()
